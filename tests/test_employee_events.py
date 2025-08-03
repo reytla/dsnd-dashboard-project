d0e9eb9dfb4f07e123735e447d339c60d517c412
reytla <gvdnest@gmail.com>
2025-08-03 12:47:09 +0200

    Fixing some linting issues

--- tests/test_employee_events.py
+++ tests/test_employee_events.py
@@ -10,10 +10,16 @@ project_root = Path(__file__).resolve().parent.parent
 # to a `db_path` function
 @pytest.fixture
 def db_path():
-    """ Fixture that provides the path to the employee_events database. """   
+    """ Fixture that provides the path to the employee_events database. """ 
     # Using the `project_root` variable
     # return a pathlib object for the `employee_events.db` file
-    return project_root / 'python-package' / 'employee_events' / 'employee_events.db'
+    return (
+        project_root 
+        / 'python-package' 
+        / 'employee_events' 
+        / 'employee_events.db'
+    )   
+
 
 # Define a function called
 # `test_db_exists`
@@ -32,15 +38,20 @@ def test_db_exists(db_path):
     # at the location passed to the test_db_exists function
     assert db_path.is_file(), f"Database file does not exist at {db_path}"
 
+
 @pytest.fixture
 def db_conn(db_path):
     from sqlite3 import connect
     return connect(db_path)
 
+
 @pytest.fixture
 def table_names(db_conn):
-    name_tuples = db_conn.execute("SELECT name FROM sqlite_master WHERE type='table';").fetchall()
+    name_tuples = db_conn.execute(
+        "SELECT name FROM sqlite_master WHERE type='table';"
+        ).fetchall()
     return [x[0] for x in name_tuples]
+
 
 # Define a test function called
 # `test_employee_table_exists`
@@ -51,7 +62,11 @@ def test_employee_table_exists(table_names):
 
     # Assert that the string 'employee'
     # is in the table_names list
-    assert 'employee' in table_names, "The 'employee' table does not exist in the database."
+    assert (
+        'employee' in table_names, 
+        "The 'employee' table does not exist in the database."
+    )
+
 
 # Define a test function called
 # `test_team_table_exists`
@@ -62,7 +77,11 @@ def test_team_table_exists(table_names):
 
     # Assert that the string 'team'
     # is in the table_names list
-    assert 'team' in table_names, "The 'team' table does not exist in the database."
+    assert (
+        'team' in table_names, 
+        "The 'team' table does not exist in the database."
+    )
+
 
 # Define a test function called
 # `test_employee_events_table_exists`
@@ -73,5 +92,7 @@ def test_employee_events_table_exists(table_names):
 
     # Assert that the string 'employee_events'
     # is in the table_names list
-    assert 'employee_events' in table_names, "The 'employee_events' table does not exist in the database."
-
+    assert (
+        'employee_events' in table_names, 
+        "The 'employee_events' table does not exist in the database."
+    )
